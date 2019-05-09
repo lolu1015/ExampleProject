94dfa0697481e36b6bb4c4daf68045a05a1d1ce1
Student <lorenz95_lukas@yahoo.de>
2019-05-09 21:06:59 +0200

    first

---
+++ app/src/main/java/hs/karlsruhe/de/BeispielActivity.java
@@ -0,0 +1,71 @@
+package hs.karlsruhe.de;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class BeispielActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beispiel);
+    }
+    public void clockwise(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.myanimation);
+        image.startAnimation(animation);
+    }
+
+    public void zoom(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.clockwise);
+        image.startAnimation(animation1);
+    }
+
+    public void fade(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.fade);
+        image.startAnimation(animation1);
+    }
+
+    public void blink(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.blink);
+        image.startAnimation(animation1);
+    }
+
+    public void move(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        animation1.setDuration(10000);
+        image.startAnimation(animation1);
+        Animation animation2 =
+                AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move2);
+        animation2.setDuration(10000);
+        image.startAnimation(animation2);
+    }
+
+    public void slide(View view){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide);
+        image.startAnimation(animation1);
+    }
+
+    public void changeActivity(View v){
+        Intent intentSecond = new Intent(this, MainActivity.class);
+        startActivity(intentSecond);
+    }
+}
